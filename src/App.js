e7e191843e7d647528aee379b96f10f739cc3e1f
Pablo del Barrio <pablodbb@gmail.com>
2021-05-17 11:15:02 +0200

    Added navbar with bootstrapp - no styling done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage/ErrorPage';
+import Navigation from './components/Navigation/Navigation';
+
 
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+    <Navigation/>
       <Switch>
-        <Route exact path='/' component={Home}  />
+        {/* <Route exact path='/' component={Home}  />
         <Route exact path='/signup' component={SignUp}  />
         <Route exact path='/signup-promoter' component={SignUpDetails}  />
         <Route exact path='/signup-venue' component={SignUpDetails}  />
@@ -16,7 +19,7 @@ function App() {
         <Route exact path='/venue-profile' component={Profile}  />
         <Route exact path='/search' component={Search}  />
         <Route exact path='/results' component={Results}  />
-        <Route exact path='/venue/:id' component={VenueDetails}  />
+        <Route exact path='/venue/:id' component={VenueDetails}  /> */}
         <Route path="*" component={ErrorPage} />
       </Switch>
     </div>
--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import error from '../../resources/img/error404.png'
 
 const ErrorPage = () =>
-  <div>
+  <div className="error-page">
     <h3>404 page not found</h3>
     <p>We are sorry but the page you are looking for does not exist.</p>
     <img src={error} alt="Error Page" />
